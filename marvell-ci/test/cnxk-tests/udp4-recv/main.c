MarvellEmbeddedProcessors/marvell-dpdk

commit df8c1e1dec7facecc7372ac3946f4791e25b6dff

marvell-ci: import marvell-ci from dpdk-24.07-devel
* Import marvell-ci directory from dpdk-24.07-devel
* Add .clang-format
* Override has_hugepage in app/test/suites/meson.build
* Update meson.build to include marvell-ci/test/cnxk-tests directory
* Removed l3fwd_non_eal from marvell-ci/test/cnxk-tests/ temporarily

Signed-off-by: Nawal Kishor <nkishor@marvell.com>
Change-Id: If9102c1b98bd7d88385cae7cb5f07cea4ac3437c
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/140569
Tested-by: Ashwin Sekhar T K <asekhar@marvell.com>
Reviewed-by: Ashwin Sekhar T K <asekhar@marvell.com>

marvell-ci/test/cnxk-tests/extbuf/meson.build
@@ -1,0 +1,59 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(C) 2022 Marvell.
+
+
+# Test directory name relative to build directory.
+test_dir = meson.current_build_dir()
+
+# Test executable
+test_exec='cnxk-extbuf'
+
+# Create the test executable
+extbuf_test=executable(
+    test_exec,
+    files('extbuf.c'),
+    link_whole: link_libs,
+    dependencies: cnxk_test_dep_objs,
+    c_args: cflags,
+    install_dir: 'bin/cnxk/extbuf',
+    install: true
+)
+# Add the meson test
+test_name = 'cnxk_extbuf_tests'
+test_script = 'cnxk_extbuf.sh'
+test_args = ''
+test(
+    test_name,
+    cnxk_test_script_wrapper,
+    env : ['DPDK_TEST=' + test_name, 'TEST_DIR=' + test_dir],
+    args : [test_script, test_args],
+    is_parallel : false,
+    suite : 'cnxk-tests')
+# Copy the required scripts to build directory.
+run_command(copy_data, test_script)
+
+
+# Add the meson test
+test_name = 'cnxk_indirectbuf_tests'
+test_script = 'cnxk_indirectbuf.sh'
+test_args = ''
+test(
+    test_name,
+    cnxk_test_script_wrapper,
+    env : ['DPDK_TEST=' + test_name, 'TEST_DIR=' + test_dir],
+    args : [test_script, test_args],
+    is_parallel : false,
+    suite : 'cnxk-tests')
+# Copy the required scripts to build directory.
+run_command(copy_data, test_script)
+
+
+# Install the script
+install_data(
+    'cnxk_extbuf.sh',
+    install_mode : 'rwxr-xr-x',
+    install_dir : 'bin/cnxk/extbuf')
+install_data(
+    'cnxk_indirectbuf.sh',
+    install_mode : 'rwxr-xr-x',
+    install_dir : 'bin/cnxk/extbuf')

marvell-ci/test/cnxk-tests/mbuf_perf/mbuf_perf.c
@@ -1,0 +1,318 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2022 Marvell.
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/queue.h>
+
+#include <rte_common.h>
+#include <rte_log.h>
+#include <rte_debug.h>
+#include <rte_memory.h>
+#include <rte_launch.h>
+#include <rte_cycles.h>
+#include <rte_eal.h>
+#include <rte_per_lcore.h>
+#include <rte_lcore.h>
+#include <rte_branch_prediction.h>
+#include <rte_mempool.h>
+#include <rte_spinlock.h>
+#include <rte_malloc.h>
+#include <rte_mbuf.h>
+#include <rte_mbuf_pool_ops.h>
+
+/*
+ * Mbuf pool performance
+ * =====================
+ *
+ *    Each core get *n_keep* mbufs per bulk of *n_get_bulk*. Then,
+ *    mbufs are put back in the pool per bulk of *n_put_bulk*.
+ *
+ *    This sequence is done during TIME_S seconds.
+ *
+ *    This test is done on the following configurations:
+ *
+ *    - Cores configuration (*cores*)
+ *
+ *      - One core with cache
+ *      - Two cores with cache
+ *      - Max. cores with cache
+ *      - One core without cache
+ *      - Two cores without cache
+ *      - Max. cores without cache
+ *
+ *    - Bulk size (*n_get_bulk*, *n_put_bulk*)
+ *
+ *      - Bulk get from 1 to 32
+ *      - Bulk put from 1 to 32
+ *      - Bulk get and put from 1 to 32, compile time constant
+ *
+ *    - Number of kept objects (*n_keep*)
+ *
+ *      - 32
+ *      - 128
+ *      - 512
+ */
+
+#define N 65536
+#define MTU 1500
+#define TIME_S 5
+#define MAX_KEEP N
+#define MEMPOOL_SIZE ((rte_lcore_count()*(MAX_KEEP+RTE_MEMPOOL_CACHE_MAX_SIZE*3))-1)
+
+/* Number of pointers fitting into one cache line. */
+#define CACHE_LINE_BURST (RTE_CACHE_LINE_SIZE / sizeof(uintptr_t))
+
+#define LOG_ERR() printf("test failed at %s():%d\n", __func__, __LINE__)
+
+static uint32_t synchro;
+
+/* number of objects in one bulk operation (get or put) */
+static unsigned int n_get_bulk;
+static unsigned int n_put_bulk;
+
+/* number of mbufs retrieved from mbuf pool before putting them back */
+static unsigned int n_keep;
+
+/* true if we want to test with constant n_get_bulk and n_put_bulk */
+static int use_constant_values;
+
+/* number of enqueues / dequeues */
+struct mbuf_perf_test_stats {
+	uint64_t enq_count;
+	uint64_t alloc_cycles;
+} __rte_cache_aligned;
+
+static struct mbuf_perf_test_stats stats[RTE_MAX_LCORE];
+
+static __rte_always_inline int
+test_loop(struct rte_mempool *mp, unsigned int x_keep, unsigned int x_get_bulk,
+	  unsigned int x_put_bulk, struct rte_mbuf **mbufs)
+{
+	unsigned int lcore_id = rte_lcore_id();
+	uint64_t start_cycles;
+	unsigned int idx;
+	unsigned int i;
+	int ret;
+
+	for (i = 0; likely(i < (N / x_keep)); i++) {
+		/* get x_keep mbufs by bulk of x_get_bulk */
+		start_cycles = rte_get_timer_cycles();
+		for (idx = 0; idx < x_keep; idx += x_get_bulk) {
+			ret = rte_pktmbuf_alloc_bulk(mp, &mbufs[idx], x_get_bulk);
+			if (unlikely(ret < 0)) {
+				rte_mempool_dump(stdout, mp);
+				return ret;
+			}
+		}
+		stats[lcore_id].alloc_cycles += rte_get_timer_cycles() - start_cycles;
+
+		/* put the mbufs back by bulk of x_put_bulk */
+		for (idx = 0; idx < x_keep; idx += x_put_bulk)
+			rte_pktmbuf_free_bulk(&mbufs[idx], x_put_bulk);
+	}
+
+	return 0;
+}
+
+static int
+per_lcore_mbuf_perf_test(void *arg)
+{
+	uint64_t time_diff = 0, hz = rte_get_timer_hz();
+	unsigned int lcore_id = rte_lcore_id();
+	uint64_t start_cycles, end_cycles;
+	struct rte_mempool *mp = arg;
+	void *mbufs;
+	int ret = -1;
+
+	/* n_get_bulk and n_put_bulk must be divisors of n_keep */
+	if (((n_keep / n_get_bulk) * n_get_bulk) != n_keep ||
+	    ((n_keep / n_put_bulk) * n_put_bulk) != n_keep)
+		goto out;
+
+	memset(&stats[lcore_id], 0, sizeof(struct mbuf_perf_test_stats));
+
+	/* wait synchro for workers */
+	if (lcore_id != rte_get_main_lcore())
+		rte_wait_until_equal_32(&synchro, 1, __ATOMIC_RELAXED);
+
+	mbufs = rte_zmalloc(NULL, MAX_KEEP * sizeof(struct rte_mbuf *), RTE_CACHE_LINE_SIZE);
+	if (!mbufs)
+		goto out;
+
+	start_cycles = rte_get_timer_cycles();
+	while (time_diff/hz < TIME_S) {
+		ret = test_loop(mp, n_keep, n_get_bulk, n_put_bulk, mbufs);
+		if (ret < 0) {
+			free(mbufs);
+			goto out;
+		}
+
+		end_cycles = rte_get_timer_cycles();
+		time_diff = end_cycles - start_cycles;
+		stats[lcore_id].enq_count += N;
+	}
+	rte_free(mbufs);
+	ret = 0;
+out:
+	return ret;
+}
+
+/* launch all the per-lcore test, and display the result */
+static int
+launch_cores(struct rte_mempool *mp, unsigned int cores)
+{
+	uint64_t hz = rte_get_timer_hz();
+	unsigned int cores_save = cores;
+	unsigned int lcore_id;
+	uint64_t rate;
+	int ret;
+
+	__atomic_store_n(&synchro, 0, __ATOMIC_RELAXED);
+
+	/* reset stats */
+	memset(stats, 0, sizeof(stats));
+
+	printf("mbuf_perf_autotest cache=%-4u cores=%-2u n_get_bulk=%-2u n_put_bulk=%-2u "
+	       "n_keep=%-4u ", mp->cache_size, cores, n_get_bulk, n_put_bulk, n_keep);
+
+	if (rte_mempool_avail_count(mp) != MEMPOOL_SIZE) {
+		printf("mempool is not full\n");
+		return -1;
+	}
+
+	RTE_LCORE_FOREACH_WORKER(lcore_id) {
+		if (cores == 1)
+			break;
+		cores--;
+		rte_eal_remote_launch(per_lcore_mbuf_perf_test, mp, lcore_id);
+	}
+
+	/* start synchro and launch test on main */
+	__atomic_store_n(&synchro, 1, __ATOMIC_RELAXED);
+
+	ret = per_lcore_mbuf_perf_test(mp);
+
+	cores = cores_save;
+	RTE_LCORE_FOREACH_WORKER(lcore_id) {
+		if (cores == 1)
+			break;
+		cores--;
+		if (rte_eal_wait_lcore(lcore_id) < 0)
+			ret = -1;
+	}
+
+	if (ret < 0) {
+		printf("per-lcore test returned -1\n");
+		return -1;
+	}
+
+	rate = 0;
+	for (lcore_id = 0; lcore_id < RTE_MAX_LCORE; lcore_id++)
+		rate += (stats[lcore_id].enq_count / TIME_S);
+
+	printf("rate_persec=%-10" PRIu64, rate);
+
+	rate = 0;
+	for (lcore_id = 0; lcore_id < RTE_MAX_LCORE; lcore_id++)
+		rate += (stats[lcore_id].enq_count / (stats[lcore_id].alloc_cycles / hz));
+
+	printf(" alloc_rate_persec=%-10" PRIu64 "\n", rate);
+
+	return 0;
+}
+
+/* for a given number of core, launch all test cases */
+static int
+do_one_mbuf_perf_test(struct rte_mempool *mp, unsigned int cores)
+{
+	unsigned int bulk_tab_get[] = { 1, CACHE_LINE_BURST, 32, 0 };
+	unsigned int bulk_tab_put[] = { 1, CACHE_LINE_BURST, 32, 0 };
+	unsigned int keep_tab[] = { RTE_MEMPOOL_CACHE_MAX_SIZE / 2, RTE_MEMPOOL_CACHE_MAX_SIZE,
+				    RTE_MEMPOOL_CACHE_MAX_SIZE * 2, RTE_MEMPOOL_CACHE_MAX_SIZE * 3,
+				    MAX_KEEP, 0 };
+	unsigned int *get_bulk_ptr, *put_bulk_ptr, *keep_ptr;
+	int ret;
+
+	for (keep_ptr = keep_tab; *keep_ptr; keep_ptr++) {
+		for (get_bulk_ptr = bulk_tab_get; *get_bulk_ptr; get_bulk_ptr++) {
+			for (put_bulk_ptr = bulk_tab_put; *put_bulk_ptr; put_bulk_ptr++) {
+				use_constant_values = 0;
+				n_get_bulk = *get_bulk_ptr;
+				n_put_bulk = *put_bulk_ptr;
+				n_keep = *keep_ptr;
+				ret = launch_cores(mp, cores);
+				if (ret < 0)
+					return -1;
+			}
+		}
+	}
+	return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+	struct rte_mempool *mp_cache = NULL;
+	struct rte_mempool *mp_nocache = NULL;
+	int ret = -1;
+
+	ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		goto exit;
+
+	/* create a mbuf pool (without cache) */
+	mp_nocache = rte_pktmbuf_pool_create("perf_test_nocache", MEMPOOL_SIZE,
+					     0, 0, MTU, SOCKET_ID_ANY);
+	if (mp_nocache == NULL)
+		goto err;
+
+	/* create a mbuf pool (with cache) */
+	mp_cache = rte_pktmbuf_pool_create("perf_test_cache", MEMPOOL_SIZE,
+				      RTE_MEMPOOL_CACHE_MAX_SIZE, 0, MTU,
+				      SOCKET_ID_ANY);
+	if (mp_cache == NULL)
+		goto err;
+
+	/* performance test with 1, 2 and max cores */
+	printf("start performance test (without cache)\n");
+
+	if (do_one_mbuf_perf_test(mp_nocache, 1) < 0)
+		goto err;
+
+	if (do_one_mbuf_perf_test(mp_nocache, 2) < 0)
+		goto err;
+
+	if (do_one_mbuf_perf_test(mp_nocache, rte_lcore_count()) < 0)
+		goto err;
+
+	/* performance test with 1, 2 and max cores */
+	printf("start performance test (with cache)\n");
+
+	if (do_one_mbuf_perf_test(mp_cache, 1) < 0)
+		goto err;
+
+	if (do_one_mbuf_perf_test(mp_cache, 2) < 0)
+		goto err;
+
+	if (do_one_mbuf_perf_test(mp_cache, rte_lcore_count()) < 0)
+		goto err;
+
+	rte_mempool_list_dump(stdout);
+
+	ret = 0;
+
+err:
+	rte_mempool_free(mp_cache);
+	rte_mempool_free(mp_nocache);
+	rte_eal_cleanup();
+exit:
+	return ret;
+}
+

app/test/suites/meson.build
@@ -7,7 +7,7 @@ timeout_seconds = 600
 timeout_seconds_fast = 10
 
 test_no_huge_args = ['--no-huge', '-m', '2048']
-has_hugepage = run_command(has_hugepages_cmd, check: true).stdout().strip() != '0'
+has_hugepage = true
 message('hugepage availability: @0@'.format(has_hugepage))
 if not has_hugepage
     if arch_subdir == 'ppc'

marvell-ci/test/cnxk-tests/bphy_irq/meson.build
@@ -1,0 +1,47 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(C) 2022 Marvell.
+
+# Test name
+test_name = 'bphy_irq_tests'
+
+# Test script
+test_script = 'bphy_irq.sh'
+
+# Arguments to the test scripts
+test_args = ''
+
+# Test directory name relative to build directory.
+test_dir = meson.current_build_dir()
+
+# Test executable
+test_exec='bphy-irq'
+
+# Copy the required scripts to build directory.
+run_command(copy_data, test_script)
+
+# Create the test executable
+bphy_irq_test=executable(
+    test_exec,
+    files('bphy_irq.c'),
+    link_whole: link_libs,
+    dependencies: cnxk_test_dep_objs,
+    c_args: cflags,
+    install_dir: 'bin/cnxk/bphy_irq',
+    install: true
+)
+
+# Add the meson test
+test(
+    test_name,
+    cnxk_test_script_wrapper,
+    env : ['DPDK_TEST=' + test_name, 'TEST_DIR=' + test_dir],
+    args : [test_script, test_args],
+    is_parallel : false,
+    suite : 'cnxk-tests')
+
+
+# Install the script
+install_data(
+    'bphy_irq.sh',
+    install_mode : 'rwxr-xr-x',
+    install_dir : 'bin/cnxk/bphy_irq')

marvell-ci/test/cnxk-tests/cman_test/meson.build
@@ -1,0 +1,32 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(C) 2021 Marvell.
+
+# Test script
+test_script = 'cnxk_cman_test.sh'
+
+# Test name
+test_name = 'cnxk_cman_test_config'
+
+# Test arguments
+test_args = ''
+
+# Test directory name relative to build directory.
+test_dir = meson.current_build_dir()
+
+# Copy the required scripts to build directory.
+run_command(copy_data, test_script)
+
+# Add the meson event test
+test(
+    test_name,
+    cnxk_test_script_wrapper,
+    env : ['DPDK_TEST=' + test_name, 'TEST_DIR=' + test_dir],
+    args : [test_script, test_args],
+    is_parallel : false,
+    suite : 'cnxk-tests')
+
+# Install the sample script
+install_data(
+    'cnxk_cman_test.sh',
+    install_mode : 'rwxr-xr-x',
+    install_dir : 'bin/cnxk/cman_test')

marvell-ci/test/cnxk-tests/common/meson.build
@@ -1,0 +1,37 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2021, Marvell
+
+if not get_option('tests')
+    subdir_done()
+endif
+
+# This executable is used as a workaround to make sure that the exe_wrapper gets
+# called for scripts also. In the exe_wrapper, this executable is ignored. Note
+# that the cnxk_test_dep_objs is inherited from app/test/meson.build.
+cnxk_test_script_wrapper = executable(
+    'cnxk-test-script-wrapper',
+    files('cnxk_test_script_wrapper.c'),
+    dependencies: cnxk_test_dep_objs,
+    install: false)
+
+# Helper command to copy files to build dir
+copy_data = find_program('copy_data.py')
+
+# These sources/dependencies/includes can be used by a cnxk specific test
+# to build a lean dpdk-test like binary with its own custom command.
+dpdk_test_sources = files('../../../../app/test/test.c')
+dpdk_test_sources += files('../../../../app/test/commands.c')
+dpdk_test_sources += files('../../../../app/test/test_pmd_perf.c')
+dpdk_test_sources += files('../../../../app/test/packet_burst_generator.c')
+dpdk_test_sources += files('../../../../app/test/test_timer_secondary.c')
+dpdk_test_sources += files('../../../../app/test/test_mp_secondary.c')
+dpdk_test_sources += files('../../../../app/test/test_pdump.c')
+dpdk_test_sources += files('../../../../app/test/sample_packet_forward.c')
+dpdk_test_inc_dir = include_directories('../../../../app/test')
+dpdk_test_link = dpdk_static_libraries + dpdk_drivers
+dpdk_test_deps = cnxk_test_dep_objs + ext_deps
+
+subdir('scapy')
+subdir('pcap')
+subdir('testpmd')
+subdir('remote')

marvell-ci/test/cnxk-tests/common/pcap/meson.build
@@ -1,0 +1,39 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2021, Marvell
+
+if not dpdk_conf.has('RTE_PORT_PCAP')
+    subdir_done()
+endif
+
+executable(
+    'pcap-pkt-cnt',
+    files('pcap_pkt_cnt.c'),
+    dependencies: cnxk_test_dep_objs,
+    install: true,
+    install_dir : 'bin/cnxk/common/pcap')
+
+executable(
+    'pcap-len',
+    files('pcap_len.c'),
+    dependencies: cnxk_test_dep_objs,
+    install: true,
+    install_dir : 'bin/cnxk/common/pcap')
+
+executable(
+    'pcap-mac',
+    files('pcap_mac.c'),
+    dependencies: cnxk_test_dep_objs,
+    install: true,
+    install_dir : 'bin/cnxk/common/pcap')
+
+run_command(copy_data, 'sample.pcap')
+install_data(
+    'sample.pcap',
+    install_mode : 'rwxr-xr-x',
+    install_dir : 'bin/cnxk/common/pcap')
+
+run_command(copy_data, 'pcap.env')
+install_data(
+    'pcap.env',
+    install_mode : 'rwxr-xr-x',
+    install_dir : 'bin/cnxk/common/pcap')

commit 62e58b35ecaf120375479a0fb77c666ce410d4be

marvell-ci: extbuf: fix compilation warning
Fixes: df8c1e1dec7f ("marvell-ci: import marvell-ci from dpdk-24.07-devel")

Change-Id: Ib09320ddc324335a98459dd2a0516f554bd5f6b2
Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>

.mailmap
@@ -1105,6 +1105,7 @@ Nir Efrati <nir.efrati@intel.com>
 Nirmoy Das <ndas@suse.de>
 Nishikant Nayak <nishikanta.nayak@intel.com>
 Nithin Dabilpuram <ndabilpuram@marvell.com> <nithin.dabilpuram@caviumnetworks.com>
+Nithin Kumar Dabilpuram <ndabilpuram@marvell.com>
 Nitin Saxena <nitin.saxena@caviumnetworks.com>
 Nitzan Weller <nitzanwe@mellanox.com>
 Nizan Zorea <nizan.zorea@intel.com>

marvell-ci/test/cnxk-tests/extbuf/meson.build
@@ -8,6 +8,13 @@ test_dir = meson.current_build_dir()
 # Test executable
 test_exec='cnxk-extbuf'
 
+app_cflags = ['-Wno-address-of-packed-member']
+foreach flag:app_cflags
+    if cc.has_argument(flag)
+        cflags += flag
+    endif
+endforeach
+
 # Create the test executable
 extbuf_test=executable(
     test_exec,

marvell-ci/test/cnxk-tests/mbuf_perf/mbuf_perf.c
@@ -150,7 +150,7 @@ per_lcore_mbuf_perf_test(void *arg)
 	while (time_diff/hz < TIME_S) {
 		ret = test_loop(mp, n_keep, n_get_bulk, n_put_bulk, mbufs);
 		if (ret < 0) {
-			free(mbufs);
+			rte_free(mbufs);
 			goto out;
 		}
 

marvell-ci/test/cnxk-tests/udp4-recv/main.c
@@ -933,7 +933,10 @@ main(int argc, char **argv)
 		printf("Creating queues: nb_rxq=%d nb_txq=%u... ",
 		       nb_rx_queue, n_tx_queue);
 
-		rte_eth_dev_info_get(portid, &dev_info);
+		if (rte_eth_dev_info_get(portid, &dev_info))
+			rte_exit(EXIT_FAILURE,
+				 "Failed to get device information for port %d\n",
+				 portid);
 
 		ret = config_port_max_pkt_len(&local_port_conf, &dev_info);
 		if (ret != 0)
@@ -1065,7 +1068,11 @@ main(int argc, char **argv)
 			printf("rxq=%d,%d,%d ", portid, queueid, socketid);
 			fflush(stdout);
 
-			rte_eth_dev_info_get(portid, &dev_info);
+			if (rte_eth_dev_info_get(portid, &dev_info))
+				rte_exit(EXIT_FAILURE,
+					 "Error during getting device (port %u) info\n",
+					 portid);
+
 			rxq_conf = dev_info.default_rxconf;
 			rxq_conf.offloads = port_conf.rxmode.offloads;
 			if (!per_port_pool)
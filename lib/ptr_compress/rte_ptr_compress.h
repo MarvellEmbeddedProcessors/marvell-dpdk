DPDK/dpdk

commit 077596a4b0776d98c58787529a4858de69e605c7

ptr_compress: add pointer compression library
Add a new utility header for compressing pointers. The provided
functions can store pointers as 32-bit or 16-bit offsets.

The compression takes advantage of the fact that pointers are
usually located in a limited memory region (like a mempool).
We can compress them by converting them to offsets from a base
memory address. Offsets can be stored in fewer bytes (dictated
by the memory region size and alignment of the pointer).
For example: an 8 byte aligned pointer which is part of a 32GB
memory pool can be stored in 4 bytes.

Suggested-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
Signed-off-by: Paul Szczepanek <paul.szczepanek@arm.com>
Signed-off-by: Kamalakshitha Aligeri <kamalakshitha.aligeri@arm.com>
Reviewed-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
Reviewed-by: Nathan Brown <nathan.brown@arm.com>
Reviewed-by: Jack Bond-Preston <jack.bond-preston@foss.arm.com>
Acked-by: Morten Brørup <mb@smartsharesystems.com>

lib/ptr_compress/rte_ptr_compress.h
@@ -1,0 +1,325 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2024 Arm Limited
+ */
+
+#ifndef RTE_PTR_COMPRESS_H
+#define RTE_PTR_COMPRESS_H
+
+/**
+ * @file
+ * Pointer compression and decompression functions.
+ *
+ * When passing arrays full of pointers between threads, memory containing
+ * the pointers is copied multiple times which is especially costly between
+ * cores. These functions allow us to compress the pointers.
+ *
+ * Compression takes advantage of the fact that pointers are usually located in
+ * a limited memory region. We compress them by converting them to offsets from
+ * a base memory address. Offsets can be stored in fewer bytes.
+ *
+ * The compression functions come in two varieties: 32-bit and 16-bit.
+ *
+ * To determine how many bits are needed to compress the pointer, calculate
+ * the biggest offset possible (highest value pointer - base pointer)
+ * and shift the value right according to alignment (shift by exponent of the
+ * power of 2 of alignment: aligned by 4 - shift by 2, aligned by 8 - shift by
+ * 3, etc.). The resulting value must fit in either 32 or 16 bits. You may
+ * use the macros provided in this file to do it programmatically.
+ *
+ * For usage example and further explanation please see this library's
+ * documentation in the programming guide.
+ */
+
+#include <stdint.h>
+#include <inttypes.h>
+
+#include <rte_bitops.h>
+#include <rte_branch_prediction.h>
+#include <rte_common.h>
+#include <rte_debug.h>
+#include <rte_vect.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Calculate how many bits are required to store pointers within a given memory
+ * region as offsets. This can help decide which pointer compression functions
+ * can be used.
+ *
+ * @param mem_length
+ *   Length of the memory region the pointers are constrained to.
+ * @return
+ *   Number of bits required to store a value.
+ **/
+#define RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE(mem_length) \
+	(((uint64_t)mem_length) < 2 ? 1 : \
+		(sizeof(uint64_t) * CHAR_BIT - \
+		 rte_clz64((uint64_t)mem_length - 1)))
+
+/**
+ * Calculate how many bits in the address can be dropped without losing any
+ * information thanks to the alignment of the address.
+ *
+ * @param alignment
+ *   Memory alignment.
+ * @return
+ *   Size of shift allowed without dropping any information from the pointer.
+ **/
+#define RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT(alignment) \
+	((alignment) == 0 ? 0 : rte_ctz64((uint64_t)alignment))
+
+/**
+ * Determine if rte_ptr_compress_16_shift can be used to compress pointers
+ * that contain addresses of memory objects whose memory is aligned by
+ * a given amount and contained in a given memory region.
+ *
+ * @param mem_length
+ *   The length of the memory region that contains the objects pointed to.
+ * @param obj_alignment
+ *   The alignment of objects pointed to.
+ * @return
+ *   1 if function can be used, 0 otherwise.
+ **/
+#define RTE_PTR_COMPRESS_CAN_COMPRESS_16_SHIFT(mem_length, obj_alignment) \
+	((RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE(mem_length) - \
+	RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT(obj_alignment)) <= 16 ? 1 : 0)
+
+/**
+ * Determine if rte_ptr_compress_32_shift can be used to compress pointers
+ * that contain addresses of memory objects whose memory is aligned by
+ * a given amount and contained in a given memory region.
+ *
+ * @param mem_length
+ *   The length of the memory region that contains the objects pointed to.
+ * @param obj_alignment
+ *   The alignment of objects pointed to.
+ * @return
+ *   1 if function can be used, 0 otherwise.
+ **/
+#define RTE_PTR_COMPRESS_CAN_COMPRESS_32_SHIFT(mem_length, obj_alignment) \
+	((RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE(mem_length) - \
+	RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT(obj_alignment)) <= 32 ? 1 : 0)
+
+/**
+ * Compress pointers into 32-bit offsets from base pointer.
+ *
+ * @note It is programmer's responsibility to ensure the resulting offsets fit
+ * into 32 bits. Alignment of the structures pointed to by the pointers allows
+ * us to drop bits from the offsets. This is controlled by the bit_shift
+ * parameter. This means that if structures are aligned by 8 bytes they must be
+ * within 32GB of the base pointer. If there is no such alignment guarantee they
+ * must be within 4GB.
+ *
+ * @param ptr_base
+ *   A pointer used to calculate offsets of pointers in src_table.
+ * @param src_table
+ *   A pointer to an array of pointers.
+ * @param dest_table
+ *   A pointer to an array of compressed pointers returned by this function.
+ * @param n
+ *   The number of objects to compress, must be strictly positive.
+ * @param bit_shift
+ *   Byte alignment of memory pointed to by the pointers allows for
+ *   bits to be dropped from the offset and hence widen the memory region that
+ *   can be covered. This controls how many bits are right shifted.
+ **/
+static __rte_always_inline void
+rte_ptr_compress_32_shift(void *ptr_base, void * const *src_table,
+		uint32_t *dest_table, size_t n, uint8_t bit_shift)
+{
+	size_t i = 0;
+#if defined RTE_HAS_SVE_ACLE && !defined RTE_ARCH_ARMv8_AARCH32
+	svuint64_t v_ptr_table;
+	do {
+		svbool_t pg = svwhilelt_b64(i, n);
+		v_ptr_table = svld1_u64(pg, (uint64_t *)src_table + i);
+		v_ptr_table = svsub_x(pg, v_ptr_table, (uint64_t)ptr_base);
+		v_ptr_table = svlsr_x(pg, v_ptr_table, bit_shift);
+		svst1w(pg, &dest_table[i], v_ptr_table);
+		i += svcntd();
+	} while (i < n);
+#elif defined __ARM_NEON && !defined RTE_ARCH_ARMv8_AARCH32
+	uint64_t ptr_diff;
+	uint64x2_t v_ptr_table;
+	/* right shift is done by left shifting by negative int */
+	int64x2_t v_shift = vdupq_n_s64(-bit_shift);
+	uint64x2_t v_ptr_base = vdupq_n_u64((uint64_t)ptr_base);
+	const size_t n_even = n & ~0x1;
+	for (; i < n_even; i += 2) {
+		v_ptr_table = vld1q_u64((const uint64_t *)src_table + i);
+		v_ptr_table = vsubq_u64(v_ptr_table, v_ptr_base);
+		v_ptr_table = vshlq_u64(v_ptr_table, v_shift);
+		vst1_u32(dest_table + i, vqmovn_u64(v_ptr_table));
+	}
+	/* process leftover single item in case of odd number of n */
+	if (unlikely(n & 0x1)) {
+		ptr_diff = RTE_PTR_DIFF(src_table[i], ptr_base);
+		dest_table[i] = (uint32_t) (ptr_diff >> bit_shift);
+	}
+#else
+	uintptr_t ptr_diff;
+	for (; i < n; i++) {
+		ptr_diff = RTE_PTR_DIFF(src_table[i], ptr_base);
+		ptr_diff = ptr_diff >> bit_shift;
+		RTE_ASSERT(ptr_diff <= UINT32_MAX);
+		dest_table[i] = (uint32_t) ptr_diff;
+	}
+#endif
+}
+
+/**
+ * Decompress pointers from 32-bit offsets from base pointer.
+ *
+ * @param ptr_base
+ *   A pointer which was used to calculate offsets in src_table.
+ * @param src_table
+ *   A pointer to an array to compressed pointers.
+ * @param dest_table
+ *   A pointer to an array of decompressed pointers returned by this function.
+ * @param n
+ *   The number of objects to decompress, must be strictly positive.
+ * @param bit_shift
+ *   Byte alignment of memory pointed to by the pointers allows for
+ *   bits to be dropped from the offset and hence widen the memory region that
+ *   can be covered. This controls how many bits are left shifted when pointers
+ *   are recovered from the offsets.
+ **/
+static __rte_always_inline void
+rte_ptr_decompress_32_shift(void *ptr_base, uint32_t const *src_table,
+		void **dest_table, size_t n, uint8_t bit_shift)
+{
+	size_t i = 0;
+#if defined RTE_HAS_SVE_ACLE && !defined RTE_ARCH_ARMv8_AARCH32
+	svuint64_t v_ptr_table;
+	do {
+		svbool_t pg = svwhilelt_b64(i, n);
+		v_ptr_table = svld1uw_u64(pg, &src_table[i]);
+		v_ptr_table = svlsl_x(pg, v_ptr_table, bit_shift);
+		v_ptr_table = svadd_x(pg, v_ptr_table, (uint64_t)ptr_base);
+		svst1(pg, (uint64_t *)dest_table + i, v_ptr_table);
+		i += svcntd();
+	} while (i < n);
+#elif defined __ARM_NEON && !defined RTE_ARCH_ARMv8_AARCH32
+	uint64_t ptr_diff;
+	uint64x2_t v_ptr_table;
+	int64x2_t v_shift = vdupq_n_s64(bit_shift);
+	uint64x2_t v_ptr_base = vdupq_n_u64((uint64_t)ptr_base);
+	const size_t n_even = n & ~0x1;
+	for (; i < n_even; i += 2) {
+		v_ptr_table = vmovl_u32(vld1_u32(src_table + i));
+		v_ptr_table = vshlq_u64(v_ptr_table, v_shift);
+		v_ptr_table = vaddq_u64(v_ptr_table, v_ptr_base);
+		vst1q_u64((uint64_t *)dest_table + i, v_ptr_table);
+	}
+	/* process leftover single item in case of odd number of n */
+	if (unlikely(n & 0x1)) {
+		ptr_diff = ((uint64_t) src_table[i]) << bit_shift;
+		dest_table[i] = RTE_PTR_ADD(ptr_base, ptr_diff);
+	}
+#else
+	uintptr_t ptr_diff;
+	for (; i < n; i++) {
+		ptr_diff = ((uintptr_t) src_table[i]) << bit_shift;
+		dest_table[i] = RTE_PTR_ADD(ptr_base, ptr_diff);
+	}
+#endif
+}
+
+/**
+ * Compress pointers into 16-bit offsets from base pointer.
+ *
+ * @note It is programmer's responsibility to ensure the resulting offsets fit
+ * into 16 bits. Alignment of the structures pointed to by the pointers allows
+ * us to drop bits from the offsets. This is controlled by the bit_shift
+ * parameter. This means that if structures are aligned by 8 bytes they must be
+ * within 256KB of the base pointer. If there is no such alignment guarantee
+ * they must be within 64KB.
+ *
+ * @param ptr_base
+ *   A pointer used to calculate offsets of pointers in src_table.
+ * @param src_table
+ *   A pointer to an array of pointers.
+ * @param dest_table
+ *   A pointer to an array of compressed pointers returned by this function.
+ * @param n
+ *   The number of objects to compress, must be strictly positive.
+ * @param bit_shift
+ *   Byte alignment of memory pointed to by the pointers allows for
+ *   bits to be dropped from the offset and hence widen the memory region that
+ *   can be covered. This controls how many bits are right shifted.
+ **/
+static __rte_always_inline void
+rte_ptr_compress_16_shift(void *ptr_base, void * const *src_table,
+		uint16_t *dest_table, size_t n, uint8_t bit_shift)
+{
+
+	size_t i = 0;
+#if defined RTE_HAS_SVE_ACLE && !defined RTE_ARCH_ARMv8_AARCH32
+	svuint64_t v_ptr_table;
+	do {
+		svbool_t pg = svwhilelt_b64(i, n);
+		v_ptr_table = svld1_u64(pg, (uint64_t *)src_table + i);
+		v_ptr_table = svsub_x(pg, v_ptr_table, (uint64_t)ptr_base);
+		v_ptr_table = svlsr_x(pg, v_ptr_table, bit_shift);
+		svst1h(pg, &dest_table[i], v_ptr_table);
+		i += svcntd();
+	} while (i < n);
+#else
+	uintptr_t ptr_diff;
+	for (; i < n; i++) {
+		ptr_diff = RTE_PTR_DIFF(src_table[i], ptr_base);
+		ptr_diff = ptr_diff >> bit_shift;
+		RTE_ASSERT(ptr_diff <= UINT16_MAX);
+		dest_table[i] = (uint16_t) ptr_diff;
+	}
+#endif
+}
+
+/**
+ * Decompress pointers from 16-bit offsets from base pointer.
+ *
+ * @param ptr_base
+ *   A pointer which was used to calculate offsets in src_table.
+ * @param src_table
+ *   A pointer to an array to compressed pointers.
+ * @param dest_table
+ *   A pointer to an array of decompressed pointers returned by this function.
+ * @param n
+ *   The number of objects to decompress, must be strictly positive.
+ * @param bit_shift
+ *   Byte alignment of memory pointed to by the pointers allows for
+ *   bits to be dropped from the offset and hence widen the memory region that
+ *   can be covered. This controls how many bits are left shifted when pointers
+ *   are recovered from the offsets.
+ **/
+static __rte_always_inline void
+rte_ptr_decompress_16_shift(void *ptr_base, uint16_t const *src_table,
+		void **dest_table, size_t n, uint8_t bit_shift)
+{
+	size_t i = 0;
+#if defined RTE_HAS_SVE_ACLE && !defined RTE_ARCH_ARMv8_AARCH32
+	svuint64_t v_ptr_table;
+	do {
+		svbool_t pg = svwhilelt_b64(i, n);
+		v_ptr_table = svld1uh_u64(pg, &src_table[i]);
+		v_ptr_table = svlsl_x(pg, v_ptr_table, bit_shift);
+		v_ptr_table = svadd_x(pg, v_ptr_table, (uint64_t)ptr_base);
+		svst1(pg, (uint64_t *)dest_table + i, v_ptr_table);
+		i += svcntd();
+	} while (i < n);
+#else
+	uintptr_t ptr_diff;
+	for (; i < n; i++) {
+		ptr_diff = ((uintptr_t) src_table[i]) << bit_shift;
+		dest_table[i] = RTE_PTR_ADD(ptr_base, ptr_diff);
+	}
+#endif
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RTE_PTR_COMPRESS_H */

MAINTAINERS
@@ -1694,6 +1694,12 @@ M: Chenbo Xia <chenbox@nvidia.com>
 M: Gaetan Rivet <grive@u256.net>
 F: lib/pci/
 
+Pointer Compression
+M: Paul Szczepanek <paul.szczepanek@arm.com>
+F: lib/ptr_compress/
+F: app/test/test_ptr_compress.c
+F: doc/guides/prog_guide/ptr_compress_lib.rst
+
 Power management
 M: Anatoly Burakov <anatoly.burakov@intel.com>
 M: David Hunt <david.hunt@intel.com>

app/test/meson.build
@@ -144,6 +144,7 @@ source_file_deps = {
     'test_power_intel_uncore.c': ['power'],
     'test_power_kvm_vm.c': ['power'],
     'test_prefetch.c': [],
+    'test_ptr_compress.c': ['ptr_compress'],
     'test_rand_perf.c': [],
     'test_rawdev.c': ['rawdev', 'bus_vdev'],
     'test_rcu_qsbr.c': ['rcu', 'hash'],

app/test/test_ptr_compress.c
@@ -1,0 +1,193 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2024 Arm Limited
+ */
+
+#include "test.h"
+#include <stdint.h>
+#include <string.h>
+
+#include <rte_ptr_compress.h>
+
+#define MAX_ALIGN_EXPONENT 3
+#define MAX_PTRS 16
+#define NUM_BASES 2
+#define NUM_REGIONS 4
+#define MAX_32BIT_REGION ((uint64_t)UINT32_MAX + 1)
+#define MAX_16BIT_REGION (UINT16_MAX + 1)
+
+static int
+test_ptr_compress_params(
+	void *base,
+	uint64_t mem_sz,
+	unsigned int align_exp,
+	unsigned int num_ptrs,
+	bool use_32_bit)
+{
+	unsigned int i;
+	unsigned int align = 1 << align_exp;
+	void *ptrs[MAX_PTRS] = {0};
+	void *ptrs_out[MAX_PTRS] = {0};
+	uint32_t offsets32[MAX_PTRS] = {0};
+	uint16_t offsets16[MAX_PTRS] = {0};
+
+	for (i = 0; i < num_ptrs; i++) {
+		/* make pointers point at memory in steps of align */
+		/* alternate steps from the start and end of memory region */
+		if ((i & 1) == 1)
+			ptrs[i] = (char *)base + mem_sz - i * align;
+		else
+			ptrs[i] = (char *)base + i * align;
+	}
+
+	if (use_32_bit) {
+		rte_ptr_compress_32_shift(
+				base, ptrs, offsets32, num_ptrs, align_exp);
+		rte_ptr_decompress_32_shift(base, offsets32, ptrs_out, num_ptrs,
+				align_exp);
+	} else {
+		rte_ptr_compress_16_shift(
+				base, ptrs, offsets16, num_ptrs, align_exp);
+		rte_ptr_decompress_16_shift(base, offsets16, ptrs_out, num_ptrs,
+				align_exp);
+	}
+
+	TEST_ASSERT_BUFFERS_ARE_EQUAL(ptrs, ptrs_out, sizeof(void *) * num_ptrs,
+		"Decompressed pointers corrupted\nbase pointer: %p, "
+		"memory region size: %" PRIu64 ", alignment exponent: %u, "
+		"num of pointers: %u, using %s offsets",
+		base, mem_sz, align_exp, num_ptrs,
+		use_32_bit ? "32-bit" : "16-bit");
+
+	return 0;
+}
+
+static int
+test_ptr_compress(void)
+{
+	unsigned int j, k, n;
+	int ret = 0;
+	/* the test is run with multiple memory regions and base addresses */
+	void * const bases[NUM_BASES] = { (void *)0, (void *)UINT16_MAX };
+	/* maximum size for pointers aligned by consecutive powers of 2 */
+	const uint64_t region_sizes_16[NUM_REGIONS] = {
+		MAX_16BIT_REGION,
+		MAX_16BIT_REGION * 2,
+		MAX_16BIT_REGION * 4,
+		MAX_16BIT_REGION * 8,
+	};
+	const uint64_t region_sizes_32[NUM_REGIONS] = {
+		MAX_32BIT_REGION,
+		MAX_32BIT_REGION * 2,
+		MAX_32BIT_REGION * 4,
+		MAX_32BIT_REGION * 8,
+	};
+
+	/* main test compresses and decompresses arrays of pointers
+	 * and compares the array before and after to verify that
+	 * pointers are successfully decompressed
+	 */
+
+	for (j = 0; j < NUM_REGIONS; j++) {
+		for (k = 0; k < NUM_BASES; k++) {
+			for (n = 1; n < MAX_PTRS; n++) {
+				ret |= test_ptr_compress_params(
+					bases[k],
+					region_sizes_16[j],
+					j /* exponent of alignment */,
+					n,
+					false
+				);
+				ret |= test_ptr_compress_params(
+					bases[k],
+					region_sizes_32[j],
+					j /* exponent of alignment */,
+					n,
+					true
+				);
+				if (ret != 0)
+					return ret;
+			}
+		}
+	}
+
+	/* verify helper macro computations */
+
+	n = RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE(0);
+	TEST_ASSERT_EQUAL(n, 1,
+			"RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE "
+			"macro computation incorrect\n");
+
+	n = RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE(1);
+	TEST_ASSERT_EQUAL(n, 1,
+			"RTE_PTR_COMPREtopSS_BITS_REQUIRED_TO_STORE_VALUE "
+			"macro computation incorrect\n");
+
+	n = RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE(MAX_16BIT_REGION);
+	TEST_ASSERT_EQUAL(n, 16,
+			"RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE "
+			"macro computation incorrect\n");
+
+	n = RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE(MAX_32BIT_REGION);
+	TEST_ASSERT_EQUAL(n, 32,
+			"RTE_PTR_COMPRESS_BITS_NEEDED_FOR_POINTER_WITHIN_RANGE "
+			"macro computation incorrect\n");
+
+	n = RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT(0);
+	TEST_ASSERT_EQUAL(n, 0,
+			"RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT "
+			"macro computation incorrect\n");
+
+	n = RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT(1);
+	TEST_ASSERT_EQUAL(n, 0,
+			"RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT "
+			"macro computation incorrect\n");
+
+	n = RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT(2);
+	TEST_ASSERT_EQUAL(n, 1,
+			"RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT "
+			"macro computation incorrect\n");
+
+	n = RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT(3);
+	TEST_ASSERT_EQUAL(n, 0,
+			"RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT "
+			"macro computation incorrect\n");
+
+	n = RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT(4);
+	TEST_ASSERT_EQUAL(n, 2,
+			"RTE_PTR_COMPRESS_BIT_SHIFT_FROM_ALIGNMENT "
+			"macro computation incorrect\n");
+
+	ret = RTE_PTR_COMPRESS_CAN_COMPRESS_16_SHIFT(MAX_16BIT_REGION, 1);
+	TEST_ASSERT_EQUAL(ret, 1,
+			"RTE_PTR_COMPRESS_CAN_COMPRESS_16_SHIFT "
+			"macro computation incorrect\n");
+
+	ret = RTE_PTR_COMPRESS_CAN_COMPRESS_16_SHIFT(MAX_16BIT_REGION + 1, 1);
+	TEST_ASSERT_EQUAL(ret, 0,
+			"RTE_PTR_COMPRESS_CAN_COMPRESS_16_SHIFT "
+			"macro computation incorrect\n");
+
+	ret = RTE_PTR_COMPRESS_CAN_COMPRESS_16_SHIFT(MAX_16BIT_REGION + 1, 2);
+	TEST_ASSERT_EQUAL(ret, 1,
+			"RTE_PTR_COMPRESS_CAN_COMPRESS_16_SHIFT "
+			"macro computation incorrect\n");
+
+	ret = RTE_PTR_COMPRESS_CAN_COMPRESS_32_SHIFT(MAX_32BIT_REGION, 1);
+	TEST_ASSERT_EQUAL(ret, 1,
+			"RTE_PTR_COMPRESS_CAN_COMPRESS_32_SHIFT "
+			"macro computation incorrect\n");
+
+	ret = RTE_PTR_COMPRESS_CAN_COMPRESS_32_SHIFT(MAX_32BIT_REGION + 1, 1);
+	TEST_ASSERT_EQUAL(ret, 0,
+			"RTE_PTR_COMPRESS_CAN_COMPRESS_32_SHIFT "
+			"macro computation incorrect\n");
+
+	ret = RTE_PTR_COMPRESS_CAN_COMPRESS_32_SHIFT(MAX_32BIT_REGION + 1, 2);
+	TEST_ASSERT_EQUAL(ret, 1,
+			"RTE_PTR_COMPRESS_CAN_COMPRESS_32_SHIFT "
+			"macro computation incorrect\n");
+
+	return 0;
+}
+
+REGISTER_FAST_TEST(ptr_compress_autotest, true, true, test_ptr_compress);

doc/api/doxy-api.conf.in
@@ -71,6 +71,7 @@ INPUT                   = @TOPDIR@/doc/api/doxy-api-index.md \
                           @TOPDIR@/lib/pipeline \
                           @TOPDIR@/lib/port \
                           @TOPDIR@/lib/power \
+                          @TOPDIR@/lib/ptr_compress \
                           @TOPDIR@/lib/rawdev \
                           @TOPDIR@/lib/rcu \
                           @TOPDIR@/lib/regexdev \

lib/meson.build
@@ -14,6 +14,7 @@ libraries = [
         'argparse',
         'telemetry', # basic info querying
         'eal', # everything depends on eal
+        'ptr_compress',
         'ring',
         'rcu', # rcu depends on ring
         'mempool',

lib/ptr_compress/meson.build
@@ -1,0 +1,4 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024 Arm Limited
+
+headers = files('rte_ptr_compress.h')

MarvellEmbeddedProcessors/marvell-dpdk

commit 99fd9ea48a761404753a668e91676d78e8c99624

ptr_compress: fix build for Arm SVE enabled SoC
Fixes: 077596a4b077 ("ptr_compress: add pointer compression library")

Change-Id: I56e791f3c5eb39ea1d839c92fe3f30ab592601e7
Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>

lib/ptr_compress/rte_ptr_compress.h
@@ -134,7 +134,7 @@ rte_ptr_compress_32_shift(void *ptr_base, void * const *src_table,
 	svuint64_t v_ptr_table;
 	do {
 		svbool_t pg = svwhilelt_b64(i, n);
-		v_ptr_table = svld1_u64(pg, (uint64_t *)src_table + i);
+		v_ptr_table = svld1_u64(pg, (const uint64_t *)src_table + i);
 		v_ptr_table = svsub_x(pg, v_ptr_table, (uint64_t)ptr_base);
 		v_ptr_table = svlsr_x(pg, v_ptr_table, bit_shift);
 		svst1w(pg, &dest_table[i], v_ptr_table);
@@ -260,7 +260,7 @@ rte_ptr_compress_16_shift(void *ptr_base, void * const *src_table,
 	svuint64_t v_ptr_table;
 	do {
 		svbool_t pg = svwhilelt_b64(i, n);
-		v_ptr_table = svld1_u64(pg, (uint64_t *)src_table + i);
+		v_ptr_table = svld1_u64(pg, (const uint64_t *)src_table + i);
 		v_ptr_table = svsub_x(pg, v_ptr_table, (uint64_t)ptr_base);
 		v_ptr_table = svlsr_x(pg, v_ptr_table, bit_shift);
 		svst1h(pg, &dest_table[i], v_ptr_table);
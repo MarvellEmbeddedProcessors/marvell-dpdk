DPDK/dpdk

commit 79c469df190056eb44ff5c91c6b02d49a1b98884

raw/cnxk_rvu_lf: support getting BAR addresses
Added rte_pmd_rvu_lf_bar_get() API to get BAR address
for out of tree drivers to configure hardware.

Signed-off-by: Akhil Goyal <gakhil@marvell.com>

drivers/raw/cnxk_rvu_lf/cnxk_rvu_lf.c
@@ -103,6 +103,30 @@ rte_pmd_rvu_lf_irq_unregister(uint8_t dev_id, unsigned int irq,
 	return roc_rvu_lf_irq_unregister(roc_rvu_lf, irq, (roc_rvu_lf_intr_cb_fn)cb, data);
 }
 
+int
+rte_pmd_rvu_lf_bar_get(uint8_t dev_id, uint8_t bar_num, size_t *va, size_t *mask)
+{
+	struct roc_rvu_lf *roc_rvu_lf;
+	struct rte_rawdev *rawdev;
+
+	rawdev = rte_rawdev_pmd_get_dev(dev_id);
+	if (rawdev == NULL)
+		return -EINVAL;
+
+	roc_rvu_lf = (struct roc_rvu_lf *)rawdev->dev_private;
+
+	if (bar_num > PCI_MAX_RESOURCE ||
+			(roc_rvu_lf->pci_dev->mem_resource[bar_num].addr == NULL)) {
+		*va = 0;
+		*mask = 0;
+		return -ENOTSUP;
+	}
+	*va = (size_t)(roc_rvu_lf->pci_dev->mem_resource[bar_num].addr);
+	*mask = (size_t)(roc_rvu_lf->pci_dev->mem_resource[bar_num].len - 1);
+
+	return 0;
+}
+
 uint16_t
 rte_pmd_rvu_lf_npa_pf_func_get(void)
 {

drivers/raw/cnxk_rvu_lf/cnxk_rvu_lf_driver.h
@@ -193,6 +193,24 @@ __rte_internal
 int rte_pmd_rvu_lf_msg_process(uint8_t dev_id, uint16_t vf, uint16_t msg_id,
 			       void *req, uint16_t req_len, void *rsp, uint16_t rsp_len);
 
+/**
+ * Get BAR addresses for the RVU LF device.
+ *
+ * @param dev_id
+ *   device id of RVU LF device
+ * @param bar_num
+ *   BAR number for which address is required
+ * @param[out] va
+ *    Virtual address of the BAR. 0 if not mapped
+ * @param[out] mask
+ *    BAR address mask, 0 if not mapped
+ *
+ * @return
+ *   Returns 0 on success, negative error code otherwise
+ */
+__rte_internal
+int rte_pmd_rvu_lf_bar_get(uint8_t dev_id, uint8_t bar_num, size_t *va, size_t *mask);
+
 #ifdef __cplusplus
 }
 #endif

drivers/raw/cnxk_rvu_lf/version.map
@@ -1,6 +1,7 @@
 INTERNAL {
 	global:
 
+	rte_pmd_rvu_lf_bar_get;
 	rte_pmd_rvu_lf_irq_register;
 	rte_pmd_rvu_lf_irq_unregister;
 	rte_pmd_rvu_lf_msg_handler_register;

MarvellEmbeddedProcessors/marvell-dpdk

commit 882136993aa5145c9b2f883d592b836f579edbe5

raw/cnxk_rvu_lf: fix array out of bound
Valid bar numbers are from 0 to PCI_MAX_RESOURCE - 1.
Hence updated the check.

Fixes: 79c469df1900 ("raw/cnxk_rvu_lf: support getting BAR addresses")
Cc: stable@dpdk.org

Signed-off-by: Akhil Goyal <gakhil@marvell.com>
Change-Id: Ic6120a1d1a2fe34602187b345d7db96236151603
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/149382
Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
Base-Tests: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
Reviewed-by: Jerin Jacob <jerinj@marvell.com>

drivers/raw/cnxk_rvu_lf/cnxk_rvu_lf.c
@@ -115,8 +115,8 @@ rte_pmd_rvu_lf_bar_get(uint8_t dev_id, uint8_t bar_num, size_t *va, size_t *mask
 
 	roc_rvu_lf = (struct roc_rvu_lf *)rawdev->dev_private;
 
-	if (bar_num > PCI_MAX_RESOURCE ||
-			(roc_rvu_lf->pci_dev->mem_resource[bar_num].addr == NULL)) {
+	if (bar_num >= PCI_MAX_RESOURCE ||
+	    (roc_rvu_lf->pci_dev->mem_resource[bar_num].addr == NULL)) {
 		*va = 0;
 		*mask = 0;
 		return -ENOTSUP;